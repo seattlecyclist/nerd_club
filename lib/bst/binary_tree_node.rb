4bcf0a3f946d25f0fc6bd1950fd507c64bc7d9dc
Devin Blais <devinblais@gmail.com>
2017-03-06 14:58:29 -0800

    Balanced binary tree

--- lib/bst/binary_tree_node.rb
+++ lib/bst/binary_tree_node.rb
@@ -10,7 +10,16 @@ class BinaryTreeNode
 
   class << self
     def build_tree(array)
-      #implement
+      if (array.length == 1)
+        return BinaryTreeNode.new(array[0])
+      end
+
+      midpoint = array.length/2
+      value = array[midpoint]
+      left = build_tree(array[0...midpoint])
+      right = build_tree(array[midpoint+1..array.length])
+
+      return BinaryTreeNode.new(value, left, right)
     end
   end
 
