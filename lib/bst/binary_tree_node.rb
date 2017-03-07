2298922b1a0b9aa474c6ad2779a1e85592120f99
Ryan Burrows <rhburrows@gmail.com>
2017-03-07 11:10:00 -0800

    Binary search tree stuff

--- lib/bst/binary_tree_node.rb
+++ lib/bst/binary_tree_node.rb
@@ -1,6 +1,6 @@
 class BinaryTreeNode
 
-  attr_accessor :value,:left, :right
+  attr_accessor :value, :left, :right
 
   def initialize(value, left = nil, right = nil)
     @value = value
@@ -10,7 +10,17 @@ class BinaryTreeNode
 
   class << self
     def build_tree(array)
-      #implement
+      return nil if array.empty?
+
+      middle = array.length / 2
+      left = array.slice(0, middle)
+      right = array.slice(middle + 1, middle)
+
+      BinaryTreeNode.new(
+        array[middle],
+        self.build_tree(left),
+        self.build_tree(right)
+      )
     end
   end
 
@@ -23,10 +33,20 @@ class BinaryTreeNode
   end
 
   def find_second_largest(node = self)
-    #implement
+    return nil if right.nil? && left.nil?
+    return left.find_largest if right.nil?
+
+    r_second = right.find_second_largest
+    if r_second.nil? || r_second.value < value
+      self
+    else
+      r_second
+    end
   end
 
   def find_largest(node = self)
-    #implement
+    return self if right.nil?
+
+    right.find_largest
   end
 end
