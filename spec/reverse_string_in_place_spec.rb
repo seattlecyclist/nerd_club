8554d50c655a3d12f96a963409bc2544af1dd1fb
Ryan Burrows <rhburrows@gmail.com>
2017-03-07 10:19:10 -0800

    In place string reverse

--- spec/reverse_string_in_place_spec.rb
+++ spec/reverse_string_in_place_spec.rb
@@ -12,8 +12,25 @@ describe 'reverse a string in place' do
     expect(reverse("a wise word")).to eq("drow esiw a")
   end
 
+  it 'works with empty strings' do
+    expect(reverse("")).to eq("")
+  end
+
+  it 'works with even length strings' do
+    expect(reverse("hello!")).to eq("!olleh")
+  end
+
 end
 
 def reverse(word)
-  #implement
+  last_index = word.length - 1
+  0.upto(last_index / 2) do |i|
+    word[i], word[last_index - i] = word[last_index - i], word[i]
+  end
+
+  word
+end
+
+def better_inplace_reverse(str)
+  str.reverse!
 end
