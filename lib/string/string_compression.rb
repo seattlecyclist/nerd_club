7d510ea73917da52b3222ea55f196fe192e78d8c
Mike Porter <mike@agileedge.com>
2017-03-08 11:05:10 -0800

    designed a cleaner way of implementing this via a look ahead

--- lib/string/string_compression.rb
+++ lib/string/string_compression.rb
@@ -2,19 +2,16 @@ class StringCompression
 
   def compress(str)
     result = ''
-    current_str = str.chars.first
+    # current_str = str.chars.first
     count = 0
-    str.chars.each do |c|
-      if c != current_str
-        result += "#{current_str}#{count}"
-        current_str = c
+    # str.chars.each do |c|
+    str.chars.each_with_index do |c, i|
+      count += 1
+      if c != str[i + 1]
+        result += "#{c}#{count}"
         count = 0
       end
-      count += 1
     end
-
-    # add last element
-    result += "#{current_str}#{count}"
 
     (str.length <= result.length ? str : result)
   end
