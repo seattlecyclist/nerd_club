96dbbad659347b29f92d674a0401d2ad26057772
Mike Porter <mike@agileedge.com>
2017-03-08 10:57:02 -0800

    string compression

--- lib/string/string_compression.rb
+++ lib/string/string_compression.rb
@@ -1,7 +1,21 @@
 class StringCompression
 
   def compress(str)
-    # implement
-    str
+    result = ''
+    current_str = str.chars.first
+    count = 0
+    str.chars.each do |c|
+      if c != current_str
+        result += "#{current_str}#{count}"
+        current_str = c
+        count = 0
+      end
+      count += 1
+    end
+
+    # add last element
+    result += "#{current_str}#{count}"
+
+    (str.length <= result.length ? str : result)
   end
 end
