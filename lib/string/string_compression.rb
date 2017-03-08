461059d1c9bd42d5218c31cd70937abea458fa0f
Mike Porter <mike@agileedge.com>
2017-03-08 11:07:05 -0800

    used an array vs building strings each time

--- lib/string/string_compression.rb
+++ lib/string/string_compression.rb
@@ -1,18 +1,18 @@
 class StringCompression
 
   def compress(str)
-    result = ''
+    result = []
     # current_str = str.chars.first
     count = 0
     # str.chars.each do |c|
     str.chars.each_with_index do |c, i|
       count += 1
       if c != str[i + 1]
-        result += "#{c}#{count}"
+        result << "#{c}#{count}"
         count = 0
       end
     end
-
-    (str.length <= result.length ? str : result)
+    result_str = result.join
+    (str.length <= result_str.length ? str : result_str)
   end
 end
