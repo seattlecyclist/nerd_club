99d7bdb8c8c3b273c3924d19e242954907dfbeb3
Devin Blais <devinblais@gmail.com>
2017-03-10 15:57:18 -0800

    Recursive string permutation

--- lib/string/recursive_string_permutation.rb
+++ lib/string/recursive_string_permutation.rb
@@ -1,7 +1,19 @@
 class RecursiveStringPermutation
 
   def permute(str)
-    # implement
-    []
+    return [str] if str.length < 2
+    solution = []
+
+    char = str[-1]
+    perms = permute(str.chop)
+
+    perms.each do |word|
+      (0..word.length).each do |i|
+        copy = word.clone
+        solution << copy.insert(i, char)
+      end
+    end
+
+    solution
   end
 end
