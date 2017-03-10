b0cf73d14a615b270167b365d4a21331515adf3d
Mike Porter <mike@agileedge.com>
2017-03-10 13:54:40 -0800

    valid lock combinations implementations

--- lib/lock/lock.rb
+++ lib/lock/lock.rb
@@ -1,7 +1,14 @@
 class Lock
 
   def combinations
-    # implement
-    []
+    result = []
+    (1..12).map(&:to_s).each do |m|
+      (1..31).map(&:to_s).each do |d|
+        combination = "#{m.rjust(2, "0")}#{d.rjust(2, "0")}"
+        result << combination if (combination.chars & combination.chars).size == 4
+      end
+    end
+
+    result
   end
 end
