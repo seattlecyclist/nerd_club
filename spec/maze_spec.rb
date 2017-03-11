f2a087c5cb83e8ad28b6a671d6fcc93ca5e32192
Mike Porter <mike@agileedge.com>
2017-03-11 14:54:44 -0800

    added a second spec

--- spec/maze_spec.rb
+++ spec/maze_spec.rb
@@ -3,17 +3,42 @@ require './lib/maze/maze'
 
 
 describe Maze do
-  
-  subject {Maze.new([
-                        [0, "S", 0, 0, 0],
-                        [0, 1, 1, 1, 0],
-                        [0, 1, 0, 1, 0],
-                        [0, 1, 0, 1, "E"],
-                        [0, 0, 0, 0, 0],
-                    ])}
+  context 'first test' do
+    subject { Maze.new(@matrix) }
 
-  it 'should find a path out' do
-    expect(subject.solve).to eq [ [0, 1], [1, 1], [1, 2], [1, 3], [2, 3], [3, 3], [3, 4] ]
+    before do
+      @matrix = [
+          [0, "S", 0, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 1, 0, 1, 0],
+          [0, 1, 0, 1, "E"],
+          [0, 0, 0, 0, 0],
+      ]
+    end
+
+    it 'should find a path out' do
+      expect(subject.solve).to eq [ [0, 1], [1, 1], [1, 2], [1, 3], [2, 3], [3, 3], [3, 4] ]
+    end
+  end
+
+  context 'another' do
+    subject { Maze.new(@matrix) }
+
+    before do
+      @matrix = [
+          [0, "S", 0, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 1, 0, 0, 0],
+          [0, 1, 1, 1, "E"],
+          [0, 0, 0, 0, 0],
+      ]
+    end
+
+    it 'should find a path out' do
+      expect(subject.solve).to eq [ [0, 1], [1, 1], [2, 1], [3, 1], [3, 2], [3, 3], [3, 4] ]
+    end
+
+
   end
 end
 
