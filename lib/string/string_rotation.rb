9f87ef12067f20ae19efcc3862ab20405fdd43de
Devin Blais <devinblais@gmail.com>
2017-03-12 14:42:30 -0700

    String rotation

--- lib/string/string_rotation.rb
+++ lib/string/string_rotation.rb
@@ -5,7 +5,20 @@ class StringRotation
   end
 
   def is_rotation?(other)
-    # implement
+    return false if other.length != @str.length || @str.empty?
+
+    (0..@str.length-1).each do |i|
+      (0..@str.length-1).each do |j|
+        og_index = (i+j)%@str.length
+
+        if @str[og_index] != other[j]
+          break
+        elsif j == @str.length-1
+          return true
+        end
+      end
+    end
+
     false
   end
 end
