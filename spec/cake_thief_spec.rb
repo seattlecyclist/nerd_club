c984fbc9794323750a29121b4f297ca2a9504cdf
Mike Porter <mike@agileedge.com>
2017-03-13 12:18:25 -0700

    Added additional specs

--- spec/cake_thief_spec.rb
+++ spec/cake_thief_spec.rb
@@ -10,8 +10,12 @@ describe DuffelBag do
     end
 
     it 'should get the right answer' do
-
       expect(subject.max_duffel_bag_value( [ [7, 160], [3, 90], [2, 15] ], 20)).to eq 555
+    end
+
+
+    it 'should get the right answer2' do
+      expect(subject.max_duffel_bag_value( [ [16, 160.00], [5, 49]], 20)).to eq 190
     end
 
   end
