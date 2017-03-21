e77180603eb378a753856890041348929cf6a09b
Mike Porter <mike@agileedge.com>
2017-03-21 14:00:25 -0700

    Created a new challenge for solving a tic-tac-toe board

---
+++ lib/tic_tac_toe/game.rb
@@ -0,0 +1,17 @@
+class Game
+  PLAYER_X = 'x'
+  PLAYER_O = 'o'
+
+  def initialize
+
+  end
+
+  # move a player(o or x) to coordinates x by y along a 3x3 board using a zero based index.
+  def move(player, x, y)
+
+  end
+
+  def winner
+    nil
+  end
+end
---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+require './lib/tic_tac_toe/game'
+
+describe Game do
+  subject {Game.new}
+
+  it 'should identify a horizontal winner' do
+    subject.move 'x', 0, 0
+    subject.move 'o', 1, 0
+    expect(subject.winner).to be_nil
+
+    subject.move 'x', 0, 1
+    subject.move 'o', 1, 1
+    expect(subject.winner).to be_nil
+    subject.move 'x', 0, 2
+
+    expect(subject.winner).to eq 'x'
+  end
+
+  it 'should identify a vertical winner' do
+    subject.move 'x', 0, 0
+    subject.move 'o', 0, 1
+    expect(subject.winner).to be_nil
+    subject.move 'x', 1, 0
+    subject.move 'o', 1, 1
+    expect(subject.winner).to be_nil
+    subject.move 'x', 2, 0
+
+    expect(subject.winner).to eq 'x'
+  end
+
+  it 'should identify a diagonal down right winner' do
+    subject.move 'x', 0, 0
+    subject.move 'o', 0, 1
+    expect(subject.winner).to be_nil
+    subject.move 'x', 1, 1
+    subject.move 'o', 1, 2
+    expect(subject.winner).to be_nil
+    subject.move 'x', 2, 2
+
+    expect(subject.winner).to eq 'x'
+  end
+
+  it 'should identify a diagonal up right winner' do
+    subject.move 'x', 2, 0
+    subject.move 'o', 0, 1
+    expect(subject.winner).to be_nil
+    subject.move 'x', 1, 1
+    subject.move 'o', 1, 2
+    expect(subject.winner).to be_nil
+    subject.move 'x', 0, 2
+
+    expect(subject.winner).to eq 'x'
+  end
+end
+
+
+
