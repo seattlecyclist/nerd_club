b430365cdb9ea792ec7343fec73c6ebf75db9ba7
Mike Porter <mike@agileedge.com>
2017-03-21 11:55:49 -0700

    Updated testˆ

--- spec/trie_spec.rb
+++ spec/trie_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require './lib/trie/trie'
+require './lib/trie/node'
 
 
 describe Trie do
@@ -15,6 +16,26 @@ describe Trie do
     end
 
   end
+end
 
+describe Node do
+  context '#search' do
+    before do
+      @a = Node.new(nil, nil, 'a')
+      @p = Node.new(nil, nil, 'p')
+      @e = Node.new(nil, nil, 'e')
+      @r = Node.new(nil, nil, 'r')
+      @k = Node.new(nil, nil, 'k')
+      @a.left = @r
+      @a.right= @p
+      @p.left = @e
+      @r.left = @k
+
+    end
+
+    it 'should find a valid node' do
+      expect(@node.search('c')).to be @c
+    end
+  end
 end
 
